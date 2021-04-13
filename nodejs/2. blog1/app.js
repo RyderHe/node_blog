1515119e41117483c9800fe398c952e5e1ab2348
hryder <r37he@edu.uwaterloo.ca>
2021-04-13 01:48:49 -0400

    login - server set cookie

--- nodejs/2. blog1/app.js
+++ nodejs/2. blog1/app.js
@@ -47,6 +47,20 @@ const serverHandle = (req, res) => {
     // query
     req.query = querystring.parse(url.split("?")[1]);
 
+    // cookie
+    req.cookie = {};
+    const cookieStr = req.headers.cookie || ""; // "k1=v1;k2=v2;k3=v3"
+    cookieStr.split("; ").forEach(item => {
+        if (!item) {
+            return;
+        }
+        const arr = item.split("=");
+        const key = arr[0];
+        const val = arr[1];
+        req.cookie[key] = val;
+    });
+    console.log("req.cookie is : ", req.cookie);
+
     // post data
     getPostData(req)
     .then(postData => {
--- nodejs/2. blog1/src/router/user.js
+++ nodejs/2. blog1/src/router/user.js
@@ -1,28 +1,56 @@
-const { loginCheck } = require("../controller/user");
+const { login } = require("../controller/user");
 const { SuccessModel, ErrorModel } = require("../model/resModel");
+
+const getCookieExpires = () => {
+    const d = new Date();
+    d.setTime(d.getTime() + 24 * 60 * 60 * 1000);
+    console.log('smgmtstring is :', d.toGMTString());
+    return d.toGMTString();
+}
 
 const handleUserRouter = (req, res) => {
     const method = req.method;
 
     // login
-    if (method === "POST" && req.path === "/api/user/login") {
-        const { username, password } = req.body;
-        const result = loginCheck(username, password);
+    if (method === "GET" && req.path === "/api/user/login") {
+        // if (result) {
+        //     return new SuccessModel();
+        // } else {
+        //     return new ErrorModel("login fails");
+        // }
         
+        // const { username, password } = req.body;
+        const { username, password } = req.query;
+        const result = login(username, password);
         return result.then(data => {
             if(data.username) {
+                // set cookie, 
+                //   path=/ means valid for every router
+                //   httpOnly makes frontend cannot change cookie username
+                //   expires : set cookie expired date
+                res.setHeader(
+                    "Set-Cookie",
+                    `username=${data.username}; path=/; httpOnly; expires=${getCookieExpires()}`
+                );
                 return new SuccessModel();
             } else {
                 return new ErrorModel("login fails");
             }
         });
         
-        // if (result) {
-        //     return new SuccessModel();
-        // } else {
-        //     return new ErrorModel("login fails");
-        // }
+    }
+
+    // login test
+    if (method === "GET" && req.path === "/api/user/login-test") {
+        if (req.cookie.username) {
+            return Promise.resolve(
+                new SuccessModel({
+                    username: req.cookie.username
+                })
+            );
+        }
+        return Promise.resolve(new ErrorModel("have not login"));
     }
 }
 
-module.exports = handleUserRouter;
+module.exports = handleUserRouter;  
